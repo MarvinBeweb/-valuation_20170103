cb99faac3dc826b3a1ffa46e2bf55dff23da82a9
Avitabile <marvin.avitabilebeweb@gmail.com>
2017-01-06 11:37:36 +0100

    Mise a jour du Fichier phptest.php (Horaire Structure Question/Theme/Réponses

--- test/phptest.php
+++ test/phptest.php
@@ -7,49 +7,86 @@
     $tabquestion = array();
     $tabqcm = array();
     $a = 0;
-
-
+     $questionType = array();
+	$time = time() + (7 * 24 * 60 * 60);
+    
 while(!feof($monfichier)) 
 {
 $content = fgets($monfichier);
-	array_push($stack, $content);
-
-$replace = str_replace("##", "", $content);
-#echo $replace;
-
+array_push($stack, $content);
+$i = 0;
+$replace = str_replace("##", " ", $content);
         if(strstr($content, '##'))
         {
         array_push($tabtheme, $content);
         array_push($tabqcm, " ");
         array_push($tabquestion, " ");
         }
-
-
-        if(strstr($content, '-'))
+        if(strpos($content, "-") === 0)
         {
         array_push($tabqcm, $content);
         array_push($tabtheme, " ");
         array_push($tabquestion, " ");
         }
-
         elseif(strstr($replace, '#'))
         {
         array_push($tabquestion, $content);
         array_push($tabtheme, " ");
         array_push($tabqcm, " ");
         }
-
-    
     
     while($a < count($stack)-1)
     {
-    print_r($tabtheme[$a]);
-    print_r($tabquestion[$a]);
-    print_r($tabqcm[$a]);
+    #print_r($tabtheme[$a]);
+    #print_r($tabquestion[$a]);
+    #print_r($tabqcm[$a]);
     $a++;
     }
-
-    
-
+             
+} 
+$save = "";
+$reponse = array();
+$finalArray = array(); // tableau stockant questions + reponse
+for ($i=0; $i < count($tabquestion)-1 ; $i++) {  // Je boucle dans mon tabquestion
+	if ($tabquestion[$i] != " "){ // Si dans mon tabquestion il y a pas du vide 
+		if($tabtheme[$i-1] != " ")
+		{
+		array_push($questionType,  $tabtheme[$i-1]);
+		}
+	    array_push($questionType,  $tabquestion[$i]); //alors je met cette qestion dans questionType
+		if($tabquestion[$i+1] != " "){
+			//pas qcm
+			print_r($questionType); 
+			$save = readline("Entrez le réponse: ");
+			$save .= "\n". 'Date et heure de la réponse :   '. date('Y-m-d-h');
+			array_push($reponse,  $save);
+			array_push($finalArray, $questionType);
+			array_push($finalArray, $reponse);
+			$reponse = array();
+			$questionType = array();
+		}
+	
+	}
+	if ($tabqcm[$i+1] != " "){ // Si dans mon tabQcm il n y a pas du vide
+	    array_push($questionType, $tabqcm[$i+1]);  //alors je met cette qestion dans questionType
+	}
+	if($tabqcm[$i+1] == " "){// si tabqcm a lindex +1 est vide
+		if(count($questionType) > 0) //si la longueur du tableau est superieur a 0 on print le
+		{
+			print_r($questionType);  
+			$save = readline("Entrez le réponse: ");
+			$save .= "\n". 'Date et heure de la réponse :   '. date('Y-m-d-h');
+			array_push($reponse,  $save);
+			array_push($finalArray, $questionType);
+			array_push($finalArray, $reponse);
+			$reponse = array();
+			
+		}
+		$questionType = array(); //reinitialise le tableeau questionType
+	}
 }
+print_r($finalArray);
 ?>
+
+
+
